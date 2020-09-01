8e069468d43c1017ec567e734670a510aa88c227
Camille Sauer <camille-sauer@hotmail.fr>
2020-09-01 10:52:01 +0200

    avatar ok

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,4 +1,4 @@
 class Avatar < ApplicationRecord
   belongs_to :category
-  validates :nom, :prenom, :img_url, :bio_url, :role, presence: true
+  validates :nom, :prenom, :img_url, :bio_url, :role, :category_id, presence: true
 end
